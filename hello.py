2aebaf8996268cadbff659ab7b9ef772c846382a
macleod <macleod@macleods-MacBook-Pro.local>
2016-02-23 00:13:54 -0700

    all tabs

--- hello.py
+++ hello.py
@@ -7,18 +7,18 @@ sockets = Sockets(app)
 @sockets.route('/echo')
 def echo_socket(ws):
 	print 'something echoed'
-    while not ws.closed:
-        message = ws.receive()
-        ws.send(message)
+	while not ws.closed:
+		message = ws.receive()
+		ws.send(message)
 
 @app.route('/')
 def hello():
 	print 'something helloed'
-    return 'Hello World!'
+	return 'Hello World!'
 
 
 if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+	from gevent import pywsgi
+	from geventwebsocket.handler import WebSocketHandler
+	server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+	server.serve_forever()
