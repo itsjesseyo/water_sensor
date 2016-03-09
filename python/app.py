fdfee85fe0ccebd9012e3fa6ce5aad1d8908e369
macleod <macleod@macleods-MacBook-Pro.local>
2016-03-08 23:11:07 -0700

    more clear example for testing

--- python/app.py
+++ python/app.py
@@ -1,10 +1,54 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
-import json as JSON
+import json as JSOND
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+
+#added specifically to allow simple angular templatesin our jinja templates
+from jinja2 import evalcontextfilter, Undefined, is_undefined
+
+class TriangleUndefined(Undefined):
+	"""
+	Implements a specific `Undefined` class.
+	If a variable in your Jinja2 template does not exists, trying to access a
+	property will raise an `AttributeError`. This class implements a mechanism
+	to prevent this behavior and return a `TriangleUndefined` object when you
+	try to access a property of a `TriangleUndefined` object.
+	The full name of a property is stored to keep a track of it :
+		>>> test = TriangleUndefined(name=test) # this is how it's initialized
+		>>> test.demo.for.the.documentation
+		Undefined
+		>>> test.demo.for.the.documentation._undefined_name
+		u'test.demo.for.the.documentation'
+	This "magic" class is used by the `angular_filter` to let the user easily
+	define its client side template.
+	"""
+
+	def __getattr__(self, name):
+		if name[1] == '_':
+			raise AttributeError(name)
+		return TriangleUndefined(name='{}.{}'.format(self._undefined_name,name))
+
+def angular_filter(value):
+	"""
+	A filter to tell Jinja2 that a variable is for the AngularJS template
+	engine.
+	If the variable is undefined, its name will be used in the AngularJS
+	template, otherwise, its content will be used.
+	"""
+
+	if is_undefined(value):
+		return '{{{{{}}}}}'.format(value._undefined_name)
+	if type(value) is bool:
+		value = repr(value).lower()
+	print 'angualr filter'
+	return '{{{{{}}}}}'.format(value)
+
+
+app.jinja_env.undefined = TriangleUndefined
+app.jinja_env.filters['angular'] = angular_filter
 socketio = SocketIO(app)
 
 ######################################################
@@ -38,6 +82,8 @@ socketio = SocketIO(app)
 #################### OUR APP #########################
 ######################################################
 
+client_list = []
+
 #hacking together a barebones angular example from this : https://realpython.com/blog/python/flask-by-example-integrating-flask-and-angularjs/
 @app.route('/home')
 def index():
@@ -46,18 +92,32 @@ def index():
 @socketio.on('connect')
 def connect():
 	print 'someone has joined the server'
-	emit('get_moisture_value', 'please') 
 
 # this will come from client
 @socketio.on('ping_server')
 def ping_server(json):
 	print('client says: ' + str(json))
+	if json['data'] not in client_list:
+		client_list.append(json['data'])
+	emit('broadcast_client_list', client_list, broadcast=True)
 
 # this will come from client
 @socketio.on('receiving_sensor_update')
 def receiving_sensor_update(json):
 	print('sensor says: ' + str(json))
 
+
+# this will come from client
+@socketio.on('atime')
+def atime(message):
+	print('sensor says: ' + str(message))
+	emit('atime', message, broadcast=True)
+
+# this will come from client
+@socketio.on('JSON')
+def JSON(json):
+	print('sensor says: ' + str(json))
+	emit('JSON', json, broadcast=True)
 
 
 if __name__ == '__main__':
--- python/static/angular_app.js
+++ python/static/angular_app.js
@@ -11,13 +11,45 @@
 	//a single contnorller can be used on many html pages, but it gets loaded as a unique instace.
 	app.controller('MyController', function($scope){
 
+		$scope.socket_clients = [];
+		$scope.my_time = '';
+		$scope.my_json = '';
+
 		//the ng-click attribute was added in html to trigger this function
 		//this way your javascript is cleanyl separated.
 		//the downside is you have to track your special html attributes
 		$scope.PingButtonClicked = function(){
 			console.log("button clicked");
-			socket.emit('ping_server', {data: 'sup bizzle'});
+			socket.emit('ping_server', {data: 'frank the website'});
 		};
+
+		$scope.FakeATime = function(){
+			console.log("button clicked");
+			socket.emit('atime', "KJ$##)CN");
+		};
+
+		$scope.FakeJSON = function(){
+			console.log("button clicked");
+			socket.emit('JSON', {data: 'frank the website'});
+		};
+
+		socket.on('broadcast_client_list', function(data) {
+			console.log('updating client list:' + data);
+			$scope.socket_clients = data;
+			$scope.$apply();
+		});
+
+		socket.on('atime', function(data) {
+			console.log('caught time broadcast' + data);
+			$scope.my_time = data;
+			$scope.$apply();
+		});
+
+		socket.on('JSON', function(data) {
+			console.log('caught json broadcast' + data['data']);
+			$scope.my_json = JSON.stringify(data);
+			$scope.$apply();
+		});
 
 	});
 
